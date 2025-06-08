647c7080119bcb092f793488303d9292e7f97677
SeulGeek <seulgi.s.life777@gmail.com>
2025-06-09 01:25:42 +0900

    feat: Update app title and display name to "Cat Breeds"

--- lib/app.dart
+++ lib/app.dart
@@ -7,7 +7,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Cat Breeds App',
+      title: 'Cat Breeds',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
